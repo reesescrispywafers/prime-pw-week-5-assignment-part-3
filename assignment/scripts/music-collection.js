0b3dc505bc5f2a0de602844110a46bea6edcfaf7
Emry Brisky <eabrisky@gmail.com>
2021-05-02 00:02:20 -0500

    working on addToCollection function

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,3 +1,67 @@
-console.log('***** Music Collection *****')
+console.log('***** Music Collection *****');
+
+// - Create a variable `collection` that starts as an empty array.
 
 let collection = [];
+
+// - Add a function named `addToCollection`. This function should:
+// - Take in the album's `title`, `artist`, `yearPublished` as input
+//   parameters
+// - Create a new object having the above properties
+// - Add the new object to the end of the `collection` array
+// - Return the newly created object
+
+function addToCollection() {
+  let album = {
+    title: '',
+    artist: '',
+    yearPublished: ''
+  };
+  collection.push(album);
+  return object;
+}
+
+// - Test the `addToCollection` function:
+// - Add 6 albums to your collection.
+//   Aim to have a mix of both same and different artists and
+//   published years. (Feel free to share your musical interests,
+//   or make stuff up. Totally fine either way.)
+// - Console.log each album as added using the returned value.
+// - After all are added, console.log the `collection` array.
+
+
+
+// - Add a function named `showCollection`. This function should:
+//   Take in an array parameter.
+//   (This allows it to be reused to show any collection,
+//   like the results from the find or search.)
+// - Console.log the number of items in the array.
+// - Loop over the array and console.log each album's information
+//   formatted like: `TITLE by ARTIST, published in YEAR`.
+
+function showCollection() {
+  
+}
+
+// - Test the `showCollection` function.
+
+
+
+// - Add a function named `findByArtist`. This function should:
+// - Take in `artist` (a string) parameter
+// - Create an array to hold any results, empty to start
+// - Loop through the `collection` and add any objects with a
+//   matching artist to the array.
+// - Return the array with the matching results.
+//   If no results are found, return an empty array.
+
+
+
+// - Test the `findByArtist` function.
+//   Make sure to test with an artist you know is in the collection,
+//   as well as an artist you know is not in your collection.
+//   Check that for artists with multiple matches, all are found.
+
+
+
+// ### STRETCH GOALS ###
