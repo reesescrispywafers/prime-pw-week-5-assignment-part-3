0c147875dc6dae134348de01fa434d3e866ff91a
Emry Brisky <eabrisky@gmail.com>
2021-05-02 16:18:33 -0500

    finished assignment commit without stretch goals

--- assignment/scripts/music-collection.js
+++ assignment/scripts/music-collection.js
@@ -1,4 +1,4 @@
-console.log('***** Music Collection *****');
+console.log(`***** Music Collection *****`);
 
 // - Create a variable `collection` that starts as an empty array.
 
@@ -11,15 +11,15 @@ let collection = [];
 // - Add the new object to the end of the `collection` array
 // - Return the newly created object
 
-function addToCollection() {
-  let album = {
-    title: '',
-    artist: '',
-    yearPublished: ''
+function addToCollection(title, artist, yearPublished) {
+  album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
   };
   collection.push(album);
-  return object;
-}
+  return album;
+} //end addToCollection
 
 // - Test the `addToCollection` function:
 // - Add 6 albums to your collection.
@@ -29,7 +29,13 @@ function addToCollection() {
 // - Console.log each album as added using the returned value.
 // - After all are added, console.log the `collection` array.
 
-
+addToCollection(`Big Soup Time`, `The Ambivalent Soup`, `1994`);
+addToCollection(`Cup O' egg day`, `Transient Tim & His Tiny Truck Trio`, `1966`);
+addToCollection(`Where Is My Soup`, `The Ambivalent Soup`, `1997`);
+addToCollection(`Frizzle Fry (Remastered)`, `Primus`, `1990`);
+addToCollection(`I Don't Know Where Your Soup Is, Please Let Go Of Me`, `The Band Formerly Known As 'The Ambivalent Soup'`, `1998`);
+addToCollection(`Stand Out - EP`, `Powerline`, `1995`);
+console.log(collection);
 
 // - Add a function named `showCollection`. This function should:
 //   Take in an array parameter.
@@ -39,13 +45,19 @@ function addToCollection() {
 // - Loop over the array and console.log each album's information
 //   formatted like: `TITLE by ARTIST, published in YEAR`.
 
-function showCollection() {
-  
+function showCollection(array) {
+  console.log(`Number of artists in collection:`, array.length);
+  for (let i = 0; i< array.length; i++) {
+    // i = 0 means our i variable will be starting at the 0 position of the array
+    console.log(array[i].title, `by`, array[i].artist, `published in`, array[i].yearPublished);
+  }
 }
+
+// what should I return so I don't get 'undefined' at the end of my loop?
 
 // - Test the `showCollection` function.
 
-
+console.log(showCollection(collection));
 
 // - Add a function named `findByArtist`. This function should:
 // - Take in `artist` (a string) parameter
@@ -55,13 +67,29 @@ function showCollection() {
 // - Return the array with the matching results.
 //   If no results are found, return an empty array.
 
-
+function findByArtist(artist) {
+  let artistArray = [];
+  if (typeof artist === 'string'){
+    for (let check of collection) {
+      if (check.artist === artist) {
+        artistArray.push(check);
+      } // end artist match check
+    } // end for of loop checking Collection
+  } else {
+    console.log(`OOPS!!!! ARTIST NOT FOUND!!!!!!!!!!`, artistArray);
+  }// end else statement
+    return artistArray;
+}
 
 // - Test the `findByArtist` function.
 //   Make sure to test with an artist you know is in the collection,
 //   as well as an artist you know is not in your collection.
 //   Check that for artists with multiple matches, all are found.
 
+console.log(findByArtist(`The Ambivalent Soup`));
+console.log(findByArtist(`Prince`));
 
+// Why does my console.log(`OOPS!!!! ARTIST NOT FOUND!!!!!!!!!!`, artistArray);
+// Not show up in the console for the `Prince` parameter input?
 
 // ### STRETCH GOALS ###
